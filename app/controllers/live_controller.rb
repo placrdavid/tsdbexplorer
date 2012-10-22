780a1477dc83874e09ae441b49097dd39d052889
David Mountain <david.mountain@placr.co.uk>
2012-10-22 16:53:59 +0100

    adding atoc and service code to outpu for live trains

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -160,19 +160,14 @@ class LiveController < ApplicationController
          tiplocs_final_array.push(tiploc)
       end
 
-      # get timetables.... for same...    
-      #@schedule = Location.where(:tiploc_code => tiplocs_string)
-
        if (order_by == 'planned_arrival_timestamp' or order_by == 'predicted_arrival_timestamp')
-         @schedule = Location.where(:tiploc_code => tiplocs_final_array).order(:public_arrival).includes(:tiploc)
+         @schedule = Location.where(:tiploc_code => tiplocs_final_array).order(:public_arrival)
        else
-         @schedule = Location.where(:tiploc_code => tiplocs_final_array).order(:public_departure).includes(:tiploc)
+         @schedule = Location.where(:tiploc_code => tiplocs_final_array).order(:public_departure)
        end
       
 
-
       # Only display passenger schedules in normal mode
-#      @schedule = @schedule.only_passenger
        late_range = 2.hour
        @range = Hash.new
        now = DateTime.now
@@ -180,13 +175,6 @@ class LiveController < ApplicationController
       # order by planned_departure       
        @range[:from] = now
        @range[:to] = now + late_range
-=begin
-       if (order_by == 'planned_arrival_timestamp' or order_by == 'predicted_arrival_timestamp')
-         @schedule = @schedule.order(:public_arrival).includes(:tiploc)
-       else
-         @schedule = @schedule.order(:public_departure).includes(:tiploc)
-       end
-=end       
       @schedule = @schedule.runs_between(@range[:from], @range[:to], false)
        
       station_updates = StationUpdate.where(:tiploc_code => tiplocs_final_array).includes(:tiploc).includes(:tracked_train).
@@ -202,7 +190,7 @@ class LiveController < ApplicationController
          originloc = Location.where(:basic_schedule_uuid => bs_uuid.to_s).where(:location_type => 'LO')
          destinloc = Location.where(:basic_schedule_uuid => bs_uuid.to_s).where(:location_type => 'LT')
 
-         # could cause problems if now is after midnight
+         # TODO could cause problems if now is after midnight
          planned_update_event_day= now
          unless schedule[:obj].public_arrival.nil?
             planned_arrival_hhmm = schedule[:obj].public_arrival
@@ -217,31 +205,18 @@ class LiveController < ApplicationController
             planned_departure_ts = Time.utc(planned_ds_departure_day.year,planned_ds_departure_day.month,planned_ds_departure_day.day,planned_departure_hhmm[0,2].to_i,  planned_departure_hhmm[2,2].to_i)               
          end
 
-      # check if we have a matching station update, if so, add it..
-#      matching_station_update_i = -1
-
-#      station_updates_i = station_updates.where('tracked_train.origin_name' => originloc[0].tiploc.tps_description)
-#      puts '=================================================='
-#      puts 'station_updates = ' + station_updates.count.to_s
       # search for a matching update on departure/arrival time
       if planned_departure_ts !=nil
          station_updates_matches = station_updates.where(:planned_departure_timestamp => planned_departure_ts)
-#         puts 'planned_departure_ts = '+planned_departure_ts.to_s
-#         puts 'station_updates_matches = ' + station_updates_matches.count.to_s
       elsif  planned_arrival_ts !=nil 
          station_updates_matches = station_updates.where(:planned_arrival_timestamp => planned_arrival_ts)
-#         puts 'planned_arrival_ts = '+planned_arrival_ts.to_s
-#         puts 'station_updates_matches = '  + station_updates_matches.count.to_s
       end
        
-matching_station_update = nil
+      matching_station_update = nil
       # now find matches on origin
       station_updates_matches.each do |station_updates_match|
-#         puts 'got matches'
          if station_updates_match.tracked_train.origin_name == originloc[0].tiploc.tps_description
             matching_station_update = station_updates_match
-#            puts 'matching_station_update..'
-#            p matching_station_update
          end
       end
       
@@ -265,36 +240,14 @@ matching_station_update = nil
          timetable_hash['event_type'] = matching_station_update.event_type unless matching_station_update.nil?         
          timetable_hash['variation_status'] = 'NO REPORT'         
          timetable_hash['variation_status'] = matching_station_update.variation_status       unless matching_station_update.nil?            
-         timetable_hash['service_name'] = schedule[:service_code]         
-         timetable_hash['operator_ref'] = schedule[:atoc_code]
-         timetables_array << timetable_hash
-         
+         timetable_hash['operator_ref'] = nil
+         timetable_hash['service_name'] = nil
+         timetable_hash['service_name'] = schedule[:obj].basic_schedule.service_code unless schedule[:obj].basic_schedule.nil?
+         timetable_hash['operator_ref'] = schedule[:obj].basic_schedule.atoc_code unless schedule[:obj].basic_schedule.nil?
+         timetables_array << timetable_hash         
       end
 
-=begin
-      updates_array=[]                 
-      # for each update, for this station, construct an array of hashes
-      station_updates.each do |station_update|
-   
-#         puts 'live departure'
-         update_hash = {}
-         update_hash['tiploc_code'] = station_update.tiploc_code
-         update_hash['station_name'] = station_update.tiploc.tps_description
-         update_hash['platform'] = station_update.platform
-         update_hash['origin_name'] = station_update.tracked_train.origin_name
-         update_hash['destination_name'] = station_update.tracked_train.destination_name
-         update_hash['diff_from_timetable_secs'] = station_update.diff_from_timetable_secs
-         update_hash['planned_arrival_timestamp'] = station_update.planned_arrival_timestamp
-         update_hash['predicted_arrival_timestamp'] = station_update.predicted_arrival_timestamp
-         update_hash['planned_departure_timestamp'] = station_update.planned_departure_timestamp
-         update_hash['predicted_departure_timestamp'] = station_update.predicted_departure_timestamp
-         update_hash['event_type'] = station_update.event_type
-         update_hash['variation_status'] = station_update.variation_status         
-         update_hash['service_name'] = station_update.tracked_train.train_service_code
-         update_hash['operator_ref'] = station_update.tracked_train.atoc_code
-         updates_array << update_hash
-      end
-=end
+
       # formulate a hash response
       @response = {}
       @response['tiploc_code'] = tiplocs_final_array
