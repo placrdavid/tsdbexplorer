561b6d08b34edc6abde522823761717071a1061c
David Mountain <david.mountain@placr.co.uk>
2012-11-14 16:04:51 +0000

    flattening locations and basic schedules table to speed up retrieval of origina and destination - sorry Mr Codd

---
+++ db/migrate/20121114153207_flatten_origin_destination_to_basicscheds_table.rb
@@ -0,0 +1,31 @@
+class FlattenOriginDestinationToBasicschedsTable < ActiveRecord::Migration
+   def up
+
+      add_column :basic_schedules, :origin_tiploc, :string, :limit => 7 
+      add_column :basic_schedules, :origin_name, :string, :limit => 26 
+      add_column :basic_schedules, :destin_tiploc, :string, :limit => 7 
+      add_column :basic_schedules, :destin_name, :string, :limit => 26
+
+      execute <<-SQL
+      update basic_schedules 
+         set origin_tiploc = locations.tiploc_code, origin_name = tiplocs.tps_description
+         from locations, tiplocs
+         where basic_schedules.uuid like locations.basic_schedule_uuid
+         and locations.tiploc_code like tiplocs.tiploc_code
+         and locations.location_type = 'LO';
+      update basic_schedules 
+         set destin_tiploc = locations.tiploc_code, destin_name = tiplocs.tps_description
+         from locations, tiplocs
+         where basic_schedules.uuid like locations.basic_schedule_uuid
+         and locations.tiploc_code like tiplocs.tiploc_code
+         and locations.location_type = 'LT';
+      SQL
+   end
+
+   def down
+      remove_column :basic_schedules, :origin_tiploc
+      remove_column :basic_schedules, :origin_name
+      remove_column :basic_schedules, :destin_tiploc
+      remove_column :basic_schedules, :destin_name
+   end
+end
