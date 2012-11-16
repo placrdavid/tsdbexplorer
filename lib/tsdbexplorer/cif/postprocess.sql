c0cc3f2f5f31474adcc68e83bbc980f1bccba56c
David Mountain <david.mountain@placr.co.uk>
2012-11-16 14:36:37 +0000

    sql script for postprocessing basic_schedules table

---
+++ lib/tsdbexplorer/cif/postprocess.sql
@@ -0,0 +1,16 @@
+﻿ALTER TABLE basic_schedules DISABLE TRIGGER ALL;
+BEGIN;
+update basic_schedules 
+   set origin_tiploc = locations.tiploc_code, origin_name = tiplocs.tps_description
+   from locations, tiplocs
+   where basic_schedules.uuid = locations.basic_schedule_uuid
+   and locations.tiploc_code = tiplocs.tiploc_code
+   and locations.location_type = 'LO';
+update basic_schedules 
+   set destin_tiploc = locations.tiploc_code, destin_name = tiplocs.tps_description
+   from locations, tiplocs
+   where basic_schedules.uuid = locations.basic_schedule_uuid
+   and locations.tiploc_code = tiplocs.tiploc_code
+   and locations.location_type = 'LT';
+COMMIT;
+﻿ALTER TABLE basic_schedules ENABLE TRIGGER ALL;
