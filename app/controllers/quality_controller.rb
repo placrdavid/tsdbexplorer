cc8634084dd1a3613a0942cf625471a1409bf25b
root <root@placraa6.miniserver.com>
2013-07-09 15:02:37 +0100

    explicitly defining endpoint in routes file

--- app/controllers/quality_controller.rb
+++ app/controllers/quality_controller.rb
@@ -68,7 +68,7 @@ class QualityController < ApplicationController
       end
       # report as json
       test_results_hash = Hash.new
-      test_results_hash = { :test => 'rate of missing locations', :actual_n => actual_n, 
+      test_results_hash = { :test => 'rate of missing basic schedules', :actual_n => actual_n, 
       	:expected_n => expected_n, :rate_of_missing => rate_of_missing, 
       	:acceptable_metric_value => acceptable_metric_value, :pass => pass }
       output_json = test_results_hash.to_json
@@ -134,34 +134,12 @@ class QualityController < ApplicationController
       end
       # report as json
       test_results_hash = Hash.new
-      test_results_hash = { :test => 'rate of excess locations', :actual_n => actual_n, 
+      test_results_hash = { :test => 'rate of excess basic schedules', :actual_n => actual_n, 
       	:expected_n => expected_n, :rate_of_excess => rate_of_excess, 
       	:acceptable_metric_value => acceptable_metric_value, :pass => pass }
       output_json = test_results_hash.to_json
       send_data output_json, :type => :json, :disposition => 'inline'
    end
 
-=begin
-# Quality test - rate of excess basic_schedules
-   def orphaned_tiplocs_in_locations_table
-      # expected n, from yml
-      expected_n = $CONFIG["COMPLETENESS_TESTS_EXPECTED_VALUES"]['expected_n_basic_schedules']
-      # actual n, from DB
-      actual_n = BasicSchedule.count
-      # rate, pass, etc
-      rate_of_excess = ((actual_n.to_f - expected_n.to_f) / expected_n.to_f).round(2)
-      acceptable_metric_value = $CONFIG["ACCEPTABLE_QUALITY_METRIC_VALUES"]['rate_of_excess_basic_schedules']
-      pass = false
-      if rate_of_excess < acceptable_metric_value
-        pass = true
-      end
-      # report as json
-      test_results_hash = Hash.new
-      test_results_hash = { :test => 'rate of excess locations', :actual_n => actual_n, 
-      	:expected_n => expected_n, :rate_of_excess => rate_of_excess, 
-      	:acceptable_metric_value => acceptable_metric_value, :pass => pass }
-      output_json = test_results_hash.to_json
-      send_data output_json, :type => :json, :disposition => 'inline'
-   end
-=end   
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,8 +31,13 @@ Tsdbexplorer::Application.routes.draw do
 
   match '/healthcheck', :controller => 'healthcheck', :action => 'index'
 
-#  match '/quality/rate_of_missing_tiplocs', :controller => 'quality', :action => 'rate_of_missing_tiplocs'
-
+   # quality metrics - completeness
+  match '/quality/rate_of_missing_tiplocs', :controller => 'quality', :action => 'rate_of_missing_tiplocs'
+  match '/quality/rate_of_missing_locations', :controller => 'quality', :action => 'rate_of_missing_locations'
+  match '/quality/rate_of_missing_basic_schedules', :controller => 'quality', :action => 'rate_of_missing_basic_schedules'
+  match '/quality/rate_of_excess_tiplocs', :controller => 'quality', :action => 'rate_of_excess_tiplocs'
+  match '/quality/rate_of_excess_locations', :controller => 'quality', :action => 'rate_of_excess_locations'
+  match '/quality/rate_of_excess_basic_schedules', :controller => 'quality', :action => 'rate_of_excess_basic_schedules'
 
   match '/location/search', :controller => 'location', :action => 'search'
   match '/location/advanced_search', :controller => 'location', :action => 'advanced_search'
